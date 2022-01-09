8f76f42a76d8700e78f259472fea6421a6e9f9e3
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-01-09 11:58:53 -0300

    first commit

---
+++ src/main/java/com/algaworks/algafood/AlgaworksAlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksAlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksAlgafoodApiApplication.class, args);
+	}
+
+}
