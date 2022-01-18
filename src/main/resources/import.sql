0770810e32813656648e3a46ca53c83553c532a9
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-01-18 15:24:38 -0300

    feat: usando specifications

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,6 +5,7 @@ insert into cozinha (id, nome) values (4, 'Coreana');
 insert into cozinha (id, nome) values (5, 'Brasileira');
 insert into cozinha (id, nome) values (6, 'Indiana');
 insert into cozinha (id, nome) values (7, 'Churrasco');
+insert into cozinha (id, nome) values (8, 'Hamburguer');
 
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Senhor Glutão', 12.50, 5);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 15.50, 1);
@@ -12,6 +13,7 @@ insert into restaurante (nome, taxa_frete, cozinha_id) values ('Larica', 3.00, 5
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('TukTuk Delivery', 1.00, 6);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('La Pururuca', 0.50, 7);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Fish and Chips', 18.50, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Burguer King', 0, 8);
 
 insert into estado (id, nome, sigla) values (1, 'Amapá', 'AP');
 insert into estado (id, nome, sigla) values (2, 'Amazonas', 'AM');
