32d10516ca9dedc7b1ae94e7c62a2b5fa71796c7
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-01-20 14:27:17 -0300

    chore: trocando auto-ddl pelo flyway

---
+++ src/main/resources/db/migration/V002__criacao-tabelas.sql
@@ -0,0 +1,17 @@
+create table estado (
+	id bigint not null auto_increment,
+	nome varchar(80) not null,
+	sigla varchar(2) not null,
+	
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
+
+create table cidade (
+	id bigint not null auto_increment,
+	nome_cidade varchar(80) not null,
+	estado varchar(80) not null,
+	
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
