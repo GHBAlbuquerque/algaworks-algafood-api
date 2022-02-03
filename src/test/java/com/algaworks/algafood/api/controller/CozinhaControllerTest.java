23c82590b0b5cf01e50c0ff5d3354e36a267fd34
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-02-02 21:27:00 -0300

    test: adicionando flyway e migrate para resetar o banco e garantir sucesso dos testes

--- src/test/java/com/algaworks/algafood/api/controller/CozinhaControllerTest.java
+++ src/test/java/com/algaworks/algafood/api/controller/CozinhaControllerTest.java
@@ -3,8 +3,12 @@ package com.algaworks.algafood.api.controller;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -15,11 +19,21 @@ public class CozinhaControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
+    @BeforeEach
+    public void setUp(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
+    }
+
     @Test
-    public void buscar_Cozinhas_Retorna200(){
+    public void listar_Cozinhas_Retorna200(){
             given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
             .when()
                 .get()
@@ -28,10 +42,8 @@ public class CozinhaControllerTest {
     }
 
     @Test
-    public void buscar_Cozinhas_Retorna200E8Itens(){
+    public void listar_Cozinhas_Retorna200E8Itens(){
             given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
             .when()
                 .get()
@@ -43,15 +55,26 @@ public class CozinhaControllerTest {
 
     @Test
     public void buscar_Cozinha_Retorna200(){
-        given()
+            given()
                 .basePath("/cozinhas/1")
-                .port(port)
                 .accept(ContentType.JSON)
-                .when()
+            .when()
                 .get()
-                .then()
+            .then()
                 .body("id", equalTo(1))
                 .body("nome", equalTo("Tailandesa"))
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void salvar_Cozinha_Retorna201(){
+        given()
+                .body("{ \"nome\": \"Chinesa\" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+            .when()
+                .post()
+            .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 }
