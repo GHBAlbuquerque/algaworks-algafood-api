0f5db5528b23691c077375951e396ee8b5599768
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-02-09 21:30:51 -0300

    feat: mudando de list para set nas entidades agregadas de restaurante

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -9,7 +9,9 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -49,17 +51,17 @@ public class Restaurante {
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     // opcional, apenas usado para customizacao
-    private List<FormaPagamento> formasPagamento;
+    private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
     @OneToMany(mappedBy = "restaurante")
-    private List<Produto> produtos = new ArrayList<>();
+    private Set<Produto> produtos = new HashSet<>();
 
     @Embedded
     private Endereco endereco;
 
     @ManyToMany
     @JoinTable(name = "restaurante_responsavel", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "usuario_id"))
-    private List<Usuario> responsaveis;
+    private Set<Usuario> responsaveis = new HashSet<>();;
 
     public void ativar() {
         setAtivo(true);
