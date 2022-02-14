23ccaaf4dd75dd6c7906aff07f77d37aad095bdc
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-02-14 11:49:25 -0300

    chore: adiciona limpador do arquivo ddl

---
+++ src/main/java/com/algaworks/algafood/core/autogeneratesql/SchemaSupport.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.core.autogeneratesql;
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Limpa auto-generated ssql.sql antes do JPA gerá-lo.
+ */
+@Configuration
+public class SchemaSupport {
+
+    @Bean
+    public static BeanFactoryPostProcessor limpadorSchemaDdlProcessor() {
+        return beanFactory -> {
+            try {
+                Files.deleteIfExists(Path.of("src/main/resources/ddl.sql"));
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+        };
+    }
+}
